8304c152a700bf0ec04d67b5f0513ed83385238a
niuzb1@gmail.com <niuzb1@gmail.com@13ed9f35-2efe-7f63-0e93-c3747f39537f>
2012-08-21 15:15:17 +0000

    ii

--- trunk/project/jni/application/doom/src/am_map.h
+++ trunk/project/jni/application/doom/src/am_map.h
@@ -36,13 +36,6 @@
 
 #include "d_event.h"
 
-#define MAPBITS 12
-#define FRACTOMAPBITS (FRACBITS-MAPBITS)
-
-// Used by ST StatusBar stuff.
-#define AM_MSGHEADER (('a'<<24)+('m'<<16))
-#define AM_MSGENTERED (AM_MSGHEADER | ('e'<<8))
-#define AM_MSGEXITED (AM_MSGHEADER | ('x'<<8))
 
 // Called by main loop.
 boolean AM_Responder (event_t* ev);
@@ -64,36 +57,7 @@ extern void AM_Start(void);
 
 //jff 4/16/98 make externally available
 
-extern void AM_clearMarks(void);
 
-typedef struct
-{
- fixed_t x,y;
-} mpoint_t;
-
-extern mpoint_t *markpoints;
-extern int markpointnum, markpointnum_max;
-
-// end changes -- killough 2/22/98
-
-// killough 5/2/98: moved from m_misc.c
-
-//jff 1/7/98 automap colors added
-extern int mapcolor_back;     // map background
-extern int mapcolor_grid;     // grid lines color
-extern int mapcolor_wall;     // normal 1s wall color
-extern int mapcolor_fchg;     // line at floor height change color
-extern int mapcolor_cchg;     // line at ceiling height change color
-extern int mapcolor_clsd;     // line at sector with floor=ceiling color
-extern int mapcolor_rkey;     // red key color
-extern int mapcolor_bkey;     // blue key color
-extern int mapcolor_ykey;     // yellow key color
-extern int mapcolor_rdor;     // red door color (diff from keys to allow option)
-extern int mapcolor_bdor;     // blue door color (of enabling one not other)
-extern int mapcolor_ydor;     // yellow door color
-extern int mapcolor_tele;     // teleporter line color
-extern int mapcolor_secr;     // secret sector boundary color
-//jff 4/23/98
 extern int mapcolor_exit;     // exit line
 extern int mapcolor_unsn;     // computer map unseen line color
 extern int mapcolor_flat;     // line with no floor/ceiling changes
